1593077565f75cba6ced26cec54910350f9d5da8
Jamie Brogdon <125220298+jamiebrogdon@users.noreply.github.com>
2024-03-09 11:57:28 -0600

    Create nestedArrays.js

---
+++ nestedArrays.js
@@ -0,0 +1,4 @@
+const numberClusters = [[1, 2],[3, 4], [5, 6]]; 
+//created a variable with 3 clusters of array elements
+const target = (numberClusters[2][1]);
+//created a variable to target the number 6 in the array 'numberClusters'. It is the second index array element, and the index of 1 within the array element
