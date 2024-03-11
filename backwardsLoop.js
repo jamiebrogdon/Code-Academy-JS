0e8b3b194a35b87fc3e4d6f888b0f79728d0e24e
Jamie Brogdon <125220298+jamiebrogdon@users.noreply.github.com>
2024-03-11 10:13:10 -0500

    Create backwardsLoop.js

---
+++ backwardsLoop.js
@@ -0,0 +1,17 @@
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+
+
+// forward counting
+for (let counter = 0; counter < 4; counter++){
+  console.log(counter);
+}
+
+// SOLUTION:
+// set the iterator variable to the highest value in the initilization expression
+// set the stopping condition to stop when the iterator var is less than 
+// the iterator should decrease instead of increase after each iteration
+
+// backward counting
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter);
+}
